df61e19d07288a66016c05be532f2fc9a38fa358
FraErriques <francesco.erriques@gmail.com>
2021-03-16 23:43:01 +0100

    minor changes, on configuration files.

--- Tim2021Console/Program.cs
+++ Tim2021Console/Program.cs
@@ -15,13 +15,13 @@ namespace Tim2021Console
 
         static void Main( string[] args )
         {
-            string basePath = ConfigurationLayer.ConfigurationService.GetSingleVoice("datafilePaths/path", "produzione");
+            string basePath = ConfigurationLayer.ConfigurationService.GetSingleVoice("datafilePaths/path", "test");
             basePath += "\\";
             ////
             //int res_BBT = Process.fromFileBulkToDb.fromFileBulk_SIMBBT_service( basePath + "SIM_BBT_.prn"); // BBT
             //int res_TIM = Process.fromFileBulkToDb.originalTIM_service( basePath + "SIM_TIM_.prn"); // TIM
             ////
-            Process.Tim2021_prova456_.Tim2021_prova456_INSERT_( basePath + "esperimento_2021#03#14_W02_.txt"); // BBT
+            Process.Tim2021_prova456_.Tim2021_prova456_INSERT_( basePath + "Example.txt"); // BBT
         }// main(
 
 
